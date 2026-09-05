5a67596b87b65d36919ddf06f7570d7919a863c4
Jepexfet-Sud <sudnikovartem21@gmail.com>
2026-09-05 17:37:23 +0300

    task

--- my_first_hw/hw2.py
+++ my_first_hw/hw2.py
@@ -1,4 +1,14 @@
 #exercise number 4
 f=open('input.txt')
+num=''
 for line in f:
-    print(line)
+    num=line
+    break
+s=open('input.txt').read()
+num=num.replace(' ',s[-1:])
+ans=str(eval(num))
+print(num)
+f=open('input.txt', 'w')
+f.write(ans)
+f.close()
+
